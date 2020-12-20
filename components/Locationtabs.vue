326dae7c0aed1b451cf84fa92bee9f65cc1c327d
Akanksha Vyas <vyasakanksha@gmail.com>
2020-12-20 16:23:34 +0530

    fixed tab closing and opening

--- components/Locationtabs.vue
+++ components/Locationtabs.vue
@@ -4,7 +4,7 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!-- linking to materialize library of icons -->
     </head>
-      
+    <v-card>
       <v-tabs
       light
       color="#0a4082"
@@ -27,20 +27,34 @@
         </v-tab-item>
 
 
-        <v-tab-item>
+        <v-tab-item eager height="550px">
           <v-img
-          src="Maplocation.jpg" >
+          src="Maplocation.jpg" 
+          lazy-src="Maplocation.jpg"
+          aspect-ratio="2.6"
+          height="400px" >
+
+          <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
           </v-img>
           <!-- map image for second tab -->
         </v-tab-item>
 
-        <v-tab-item>
-          <v-img
-          src="Satellite.jpg" >
-          </v-img>
-          <!-- satellite image for third tab -->
+        <v-tab-item eager height="550px">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109586.5980917848!2d77.90997246073701!3d27.176309822555126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39740d857c2f41d9%3A0x784aef38a9523b42!2sAgra%2C%20Uttar%20Pradesh!5e1!3m2!1sen!2sin!4v1608456887439!5m2!1sen!2sin" width="1200" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>          <!-- satellite image for third tab -->
         </v-tab-item>
       </v-tabs>
+    </v-card>
   
   </html>
 </template>
@@ -49,5 +63,6 @@
 .noscrollbar{
 overflow-y: hidden;}
 /* hiding vertical scrollbar */
+
 </style>
 
